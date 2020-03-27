9b9a013beeeae0380b1059f239a7621f119d22e8
ivanlandinez1994 <ivan.landinez.gallego@gmail.com>
2020-03-26 21:57:30 -0300

    Form Tipo Pasos

--- FormRegistro/js/application.js
+++ FormRegistro/js/application.js
@@ -110,11 +110,11 @@ function validarForm(e) {
 }
 
 //---------------------------------------- INICIO VISUALIZACIÓN TABS ---------------------------------------//
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+var currentTab = 0; // Por fecto inicia en el primer tab posicion (0)
+showTab(currentTab); // Activa la visualizacion del tab actual (El primero).
 
 function showTab(n) {
-  // This function will display the specified tab of the form ...
+  // Esta funcion muestra el tab recibido por parametro...
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   // ... and fix the Previous/Next buttons:
@@ -149,29 +149,29 @@ function nextPrev(n) {
 	}
 	// Otherwise, display the correct tab:
 	showTab(currentTab);
-  }
+}
   
-  function validateForm() {
+function validateForm() {
 	// This function deals with validation of the form fields
 	var x, y, i, valid = true;
 	x = document.getElementsByClassName("tab");
 	y = x[currentTab].getElementsByTagName("input");
 	// A loop that checks every input field in the current tab:
 	for (i = 0; i < y.length; i++) {
-	  // If a field is empty...
-	  if (y[i].value == "") {
+	// If a field is empty...
+	if (y[i].value == "") {
 		// add an "invalid" class to the field:
 		y[i].className += " invalid";
 		// and set the current valid status to false:
 		valid = false;
-	  }
+	}
 	}
 	// If the valid status is true, mark the step as finished and valid:
 	if (valid) {
-	  document.getElementsByClassName("step")[currentTab].className += " finish";
+	document.getElementsByClassName("step")[currentTab].className += " finish";
 	}
 	return valid; // return the valid status
-  }
+}
   
   function fixStepIndicator(n) {
 	// This function removes the "active" class of all steps...
