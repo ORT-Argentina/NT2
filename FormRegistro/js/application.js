67e2575efe0bc1961ce52119004f30216fc3f088
Seba <sebastianpanetta@gmail.com>
2020-03-28 16:53:21 -0300

    Agregado de Steps 1 y 2
    - Agregado de Step 1: Datos Personales
    - Agregado de Step 2: Domicilio
    - Agregado de lógica para el render de los steps
    - Agregado de lógica para ocultar nro de trámite de DNI
    - Agregado de lógica para renderizar ciudades por provincia

--- FormRegistro/js/application.js
+++ FormRegistro/js/application.js
@@ -110,15 +110,101 @@ function validarForm(e) {
 }
 
 function continuar(step) {
-	switch(step) {
+	switch (step) {
 		case 1:
 			renderDomicilio();
-		break;
+			break;
 	}
 }
 
+// renderizar el step domicilio y ocultar el step anterior
 function renderDomicilio() {
-	let f = $("form");
-	f.empty();
-	f.append("<p> new paragraph </p>");
+	let thisStep = $("#step1");
+	thisStep.prop("hidden", true);
+	let nextStep = $("#step2");
+	nextStep.prop("hidden", false);
 }
+
+// al cambiar el combo box provincia, renderizar localidades
+$("#cmbProvincia").change(function () {
+	if ($(this).val() == "") {
+		$('#localidad').hide();
+		$('#cmbLocalidad').removeAttr('required');
+		$('#cmbLocalidad').removeAttr('data-error');
+	} else {
+		$('#localidad').show();
+		$('#cmbLocalidad').attr('required', '');
+		$('#cmbLocalidad').attr('data-error', 'This field is required.');
+
+		$("#cmbLocalidad").empty();
+		var texto = "Elija una localidad";
+		var valor = ""
+		var o = new Option(texto, valor);
+		$(o).html(texto);
+		$("#cmbLocalidad").append(o);
+
+		switch ($(this).val()) {
+			case "caba":
+				var texto = "Agronomia";
+				var valor = "agronomia"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "Almagro";
+				var valor = "almagro"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "Devoto";
+				var valor = "devoto"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "Villa Urquiza";
+				var valor = "villa-urquiza"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "Villa Ortuzar";
+				var valor = "villa-ortuzar"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				break;
+			case "buenos-aires":
+				var texto = "San Isidro";
+				var valor = "san-isidro"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "Villa Ballester";
+				var valor = "ballester"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "San Justo";
+				var valor = "san-justo"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				var texto = "Avellaneda";
+				var valor = "avellaneda"
+				var o = new Option(texto, valor);
+				$(o).html(texto);
+				$("#cmbLocalidad").append(o);
+				break;
+		}
+	}
+});
+
+// habilita o deshabilita el campo Número de trámite solamente si se selecciona DNI
+$("#cmbTipoDocumento").change(function () {
+	let tipoDoc = $(this).val();
+	let nroTramite = $("#txtNumeroTramite");
+	if (tipoDoc == "dni") {
+		nroTramite.prop("disabled",false);
+	} else {
+		nroTramite.val("");
+		nroTramite.prop("disabled",true);
+	}
+});
